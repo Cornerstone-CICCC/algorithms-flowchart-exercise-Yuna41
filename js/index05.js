1fc099ad63cc394abcdbb45eab682b2749e17f1a
Yuna Shimonosono <uncnprs@gmail.com>
2025-05-22 00:11:21 -0700

    Done Js 5

---
+++ js/index05.js
@@ -0,0 +1,12 @@
+let name = "Harry"
+let marks = 67
+
+if(marks >= 80){
+  console.log(`${name}'s grade is "A".`)
+} else if(marks >= 60){
+  console.log(`${name}'s grade is "B".`)
+} else if(marks >= 40){
+  console.log(`${name}'s grade is "C".`)
+} else {
+  console.log(`${name}'s grade is "No Grade".`)
+}
