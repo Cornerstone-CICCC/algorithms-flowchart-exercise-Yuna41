9ad93bca8ccb7090abb958d502f1d1e1604ea790
Yuna Shimonosono <uncnprs@gmail.com>
2025-05-21 21:53:26 -0700

    Done Js 3

---
+++ js/index03.js
@@ -0,0 +1,15 @@
+let num01 = 41
+let num02 = 19
+let num03 = 37
+
+if (num01 > num02){
+  if (num01 > num03){
+    console.log("num01 is the largest number.")
+  } else {
+    console.log("num03 is the largest number.")
+  }
+} else if (num02 > num03){
+  console.log("num02 is the largest number.")
+} else {
+  console.log("num03 is the largest number.")
+}
